a5fa78e7181c84fdeaa5f587c9c2afa052b6d32a
MonsieurHBA <91086501+MonsieurHBA@users.noreply.github.com>
2021-09-20 20:47:42 +0200

    Update AuthController.js

--- controller/AuthController.js
+++ controller/AuthController.js
@@ -1,12 +1,12 @@
-// hne n7ot les call back function mtaa routes eli fi authrout
+
 
 const jwt = require('jsonwebtoken') ;
 const User = require('../models/User') ;
 
 // catch err eli 3malthom f User.js
 const HandelErr = (err) => {
-console.log(err.message , err.code) ;   //err.message t5alini ne5eé les message d'erreur eli mawjoudin f User.js
-let error = {Email: '' , Password: '' } ;  // hedhom ferghyn bech n7ot fyhom el erreur o nab3athhom lel user sous forme JSON
+console.log(err.message , err.code) ;   
+let error = {Email: '' , Password: '' } ;  
 
 // incorrect email not registred
 if(err.message === 'Incorrect Email') {
@@ -28,9 +28,9 @@ if(err.code ===11000) {
 
 // validation error
 if(err.message.includes('user_pizza validation failed')){
-    Object.values(err.errors).forEach(({properties})=>{  // Object.values(err.errors) : hedhom el valeur mtaa error eli fyh error email o erreur password
-        // tawa bech nekh2o juste el message d'error o n7otouhom f error eli fyh Email o password ferghyn (forEach)
-        error[properties.path] = properties.message ;   //properties.path = Email o Password , properties.message : heya el message d'erreur
+    Object.values(err.errors).forEach(({properties})=>{  
+       
+        error[properties.path] = properties.message ;  
     })
 
     
@@ -43,7 +43,7 @@ return error ;
 const maxAge = 3 * 24 * 60 * 60
 const createToken = (id) =>{
     return jwt.sign({ id } , 'Pizza' , {
-        expiresIn: maxAge  // toufa les cookies fi 3 day
+        expiresIn: maxAge  
     })
 }
 
@@ -58,12 +58,12 @@ module.exports.login_get = (req , res) =>{
 module.exports.signup_post = async (req , res) =>{
 
     const { Email , Password  } = req.body ;
-    // tawa bech naamlo new user f data base bel proprity Email , Password
+    
     try {
-       const user = await User.create({Email , Password }) ;  // a3meli user jdid 7aseb el schema eli 3malto ena deja f User.js
+       const user = await User.create({Email , Password }) ;  
       const token = createToken(user._id);
       res.cookie('jwt' ,  token , {httpOnly: true , maxAge: maxAge * 1000})
-       res.status(201).json({user: user._id}) ;   // raja3li el user eli 3malto f data base
+       res.status(201).json({user: user._id}) ;   
     } 
     
     catch(err) {
@@ -74,7 +74,7 @@ module.exports.signup_post = async (req , res) =>{
 }
 
 module.exports.login_post = async function (req , res) {
-    //console.log(req.body)   najem n'affichi eli b3aththom fi postman avec el midelware **** app.use(express.json()) ;
+   
     const { Email , Password  } = req.body ;
     try {
         const user = await User.login(Email , Password);
@@ -82,7 +82,7 @@ module.exports.login_post = async function (req , res) {
         res.cookie('jwt' ,  token , {httpOnly: true , maxAge: maxAge * 1000})
         res.status(200).json({user: user._id}) ;
     }catch(err) {
-        const errors = HandelErr(err) ;    //throw Error('Incorrect Password') ;     throw Error ('Incorrect Email') ; hedhom ki nheb n'affichehom f console ki na3mel ghalta f email wala password<  
+        const errors = HandelErr(err) ;    
          res.status(400).json({errors}) ;
     }
 }
